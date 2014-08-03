c567fb8b8b3c25cf94f4ba50d1624eab1695f539
ilushechkin <serge@MacBook-Pro-Sergey.local>
2014-08-03 19:54:44 +0400

    Change date format and chmod added for security

--- WordPressBackup.sh
+++ WordPressBackup.sh
@@ -4,7 +4,7 @@
 # Build date 07/14/2014
 
 # Adjust this to where you websites are stored.
-FINDDIR=/home/
+FINDDIR=/var/www/
 
 # Searches for the backup.profile in the web directory.
 profile=$(find ${FINDDIR} -wholename "*backup.profile" )
@@ -14,27 +14,27 @@ echo "********************************************************************";
 echo "Using Profile: ${backupprofile}";
 . $backupprofile
                 
-	if [ "${backup_enabled}" = "1" ]; then
+    if [ "${backup_enabled}" = "1" ]; then
                         wp_config=${wp_root}/wp-config.php
                                 # Verifing that wp-config is in the location
-		  if [ ! -f "$wp_config" ]; then
-		   echo "No WP-Config.php Found Attempting to find one";
-	                   # Checks the directory above WP_root
-		   cd $wp_root
-		   wp_config=../wp-config.php
+          if [ ! -f "$wp_config" ]; then
+           echo "No WP-Config.php Found Attempting to find one";
+                       # Checks the directory above WP_root
+           cd $wp_root
+           wp_config=../wp-config.php
                                    # IF wp-configstill not found
                                    if [ ! -f "$wp_config" ]; then
-		    echo "FATAL ERROR: wp-config.php is missing from a readable state";
-		      echo "Does $wp_domain still work?";
-				 
-		     fi
-		fi
+            echo "FATAL ERROR: wp-config.php is missing from a readable state";
+              echo "Does $wp_domain still work?";
+                 
+             fi
+        fi
                                 
                                 # Verifing wp_config exists
-		if [ -f "$wp_config" ]; then
-	                # BackupName Date and time
-                                backupname=$(date +%m%d%y)
-	
+        if [ -f "$wp_config" ]; then
+                    # BackupName Date and time
+                                backupname=$(date +%d%m%Y)
+    
                                 # Pulls Database info from WP-config
                                 db_name=$(grep DB_NAME "${wp_config}" | cut -f4 -d"'")
                                 db_user=$(grep DB_USER "${wp_config}" | cut -f4 -d"'")
@@ -55,6 +55,7 @@ echo "Using Profile: ${backupprofile}";
 
                                 # Compresses the MySQL Dump and the Home Directory
                                 tar zcPf ./${wp_domain}-${backupname}.tar.gz ./${backupname}-FILES.tar.gz ./${backupname}-DB.sql.gz
+                                chmod 600 ./${wp_domain}-${backupname}.tar.gz
 
                                 # Generates the Backup Size
                                 FILENAME=${backup_location}/${user}/${wp_domain}/${wp_domain}-${backupname}.tar.gz
@@ -66,8 +67,8 @@ echo "Using Profile: ${backupprofile}";
 
                                 #Deletes any Backup older than X days
                               find ${backup_location}/${user}/${wp_domain}/ -type f -mtime +${keepdays} -exec rm {} \;
-                	fi
-		fi
+                    fi
+        fi
                 if [ "${backupenabled}" = "0" ]; then
                                 echo "Backups NOT enabled for ${wp_root}";
                 fi
@@ -75,4 +76,4 @@ done
 echo " ";
 echo "********************************************************************";
 echo "This script is licensed under GPL https://github.com/jamesrascal/wordpress-backup/";
-echo "Run Date: $(date +%m%d%y%k%M)";
+echo "Run Date: $(date +%d%m%Y%k%M)";
